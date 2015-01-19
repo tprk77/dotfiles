acbfcb1e296a39f494ae4d25e551b20dcdec4314
Tim Perkins <tprk77@gmail.com>
2015-01-18 23:01:00 -0500

    Turn off weird Ido directory switching.

--- emacs/init.el
+++ emacs/init.el
@@ -136,6 +136,7 @@
   :config (progn
             (setq ido-everywhere t
                   ido-enable-flex-matching t
+                  ido-auto-merge-work-directories-length -1
                   ido-create-new-buffer 'always)
             (ido-mode)))
 

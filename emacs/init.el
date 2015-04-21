097d97a2abfab2290d6798ea13cb91209eef2ee1
Tim Perkins <tprk77@gmail.com>
2015-04-21 10:56:14 -0400

    Added buffer-move

--- emacs/init.el
+++ emacs/init.el
@@ -267,6 +267,13 @@
   :config (popwin-mode)
   :ensure t)
 
+(use-package buffer-move
+  :bind (("C-S-<up>" . buf-move-up)
+         ("C-S-<down>" . buf-move-down)
+         ("C-S-<left>" . buf-move-left)
+         ("C-S-<right>" . buf-move-right))
+  :ensure t)
+
 (use-package undo-tree
   :defer 2
   :commands global-undo-tree-mode

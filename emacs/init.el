e261d46ea2b3e423d3acf9476adc803fc0b8ad66
Tim Perkins <tprk77@gmail.com>
2015-09-09 15:36:01 -0400

    Better movement to the beginning of lines

--- emacs/init.el
+++ emacs/init.el
@@ -87,6 +87,29 @@
 (global-set-key (kbd "<f1>") #'shell)
 (global-set-key (kbd "<f2>") #'rgrep)
 
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+;; remap C-a to `smarter-move-beginning-of-line'
+(global-set-key [remap move-beginning-of-line] 'smarter-move-beginning-of-line)
+
 ;; Put backups and autosaves in separate directory
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory))

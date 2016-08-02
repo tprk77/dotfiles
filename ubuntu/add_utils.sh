fd7574464ea9062403e08b5aefb54336363d6e3d
Tim Perkins <tprk77@gmail.com>
2016-08-02 12:34:33 -0400

    Add build tools to utilities.

--- ubuntu/add_utils.sh
+++ ubuntu/add_utils.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+general_utils="lftp tree curl sshfs"
+dev_tools="silversearcher-ag dos2unix"
+build_tools="cmake autoconf automake libtool"
+
 # add some of my favorite utilities
-sudo apt-get install -y lftp tree curl silversearcher-ag dos2unix sshfs
+sudo apt-get install -y \
+     $general_utils \
+     $dev_tools \
+     $build_tools

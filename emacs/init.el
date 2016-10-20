c8b759008369821e762d0180a8bdea4956fc865b
Tim Perkins <tprk77@gmail.com>
2016-10-20 17:36:06 -0400

    Prevent auto-revert from getting Git's index.lock
    Really lame. See Emacs bug #21559.

--- emacs/init.el
+++ emacs/init.el
@@ -151,6 +151,27 @@
 ;; Hooks to display functions in some modes
 (add-hook 'prog-mode-hook #'which-func-mode)
 
+;; Monkey patch `vc-git-conflicted-files' because it sucks!
+;; See Emacs bug #21559: http://debbugs.gnu.org/cgi/bugreport.cgi?bug=21559
+(message "Monkey patching `vc-git-conflicted-files' for Emacs bug #21559!")
+(require 'vc)
+(defun vc-git-conflicted-files (directory)
+  "Return the list of files with conflicts in DIRECTORY."
+  (let* ((status
+          (vc-git--run-command-string directory "diff-files" "--name-status"))
+         (lines (when status (split-string status "\n" 'omit-nulls)))
+         files)
+    ;; TODO: Look into reimplementing `vc-git-state', as well as
+    ;; `vc-git-dir-status-files', based on this output, thus making the
+    ;; extra process call in `vc-git-find-file-hook' unnecessary.
+    (dolist (line lines files)
+      (when (string-match "\\([ MADRCU?!]\\)[ \t]+\\(.+\\)" line)
+        (let ((state (match-string 1 line))
+              (file (match-string 2 line)))
+          ;; See git-status(1).
+          (when (equal state "U")
+            (push (expand-file-name file directory) files)))))))
+
 ;;; Package Support
 
 (let ((default-directory "~/.emacs.d/plugins/"))

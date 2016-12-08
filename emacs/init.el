1c42e8924a980b0687be65dd7a33e0ca8d4f85eb
Tim Perkins <tprk77@gmail.com>
2016-12-07 20:02:41 -0500

    Added lua-mode!

--- emacs/init.el
+++ emacs/init.el
@@ -424,6 +424,11 @@
             (yas-reload-all))
   :ensure t)
 
+(use-package lua-mode
+  :mode ("\\.lua\\'" . lua-mode)
+  :config (setq lua-indent-level 2)
+  :ensure t)
+
 (use-package js2-mode
   :mode ("\\.js\\'" . js2-mode)
   :config (progn

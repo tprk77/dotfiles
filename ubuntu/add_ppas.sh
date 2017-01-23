c1de4a6e067eac2bb0f390ca669cb6bf4697a62f
Tim Perkins <tprk77@gmail.com>
2017-01-23 17:47:57 -0500

    Format script comments

--- ubuntu/add_ppas.sh
+++ ubuntu/add_ppas.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# gets the newest Git (necessary for Emacs's Magit)
+# Gets the newest Git (necessary for Emacs's Magit)
 sudo apt-add-repository ppa:git-core/ppa
 
-# gets the latest Emacs (necessary for Emacs's Magit)
+# Gets the latest Emacs (necessary for Emacs's Magit)
 sudo add-apt-repository ppa:ubuntu-elisp/ppa
 
-# update and upgrade
+# Update and upgrade
 sudo apt-get update
 sudo apt-get upgrade -y

b2314541ef356519873c28ed8798b80cfd93be76
Tim Perkins <tprk77@gmail.com>
2017-01-23 17:47:57 -0500

    Add 7z to utilities

--- ubuntu/add_utils.sh
+++ ubuntu/add_utils.sh
@@ -3,9 +3,11 @@
 general_utils="lftp tree curl sshfs"
 dev_tools="silversearcher-ag dos2unix"
 build_tools="cmake autoconf automake libtool"
+zip_tools="p7zip-full p7zip-rar"
 
 # add some of my favorite utilities
 sudo apt-get install -y \
      $general_utils \
      $dev_tools \
-     $build_tools
+     $build_tools \
+     $zip_tools

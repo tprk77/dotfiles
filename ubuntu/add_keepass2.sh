ee3c31027ca782f8898680059c55998c24b6b49f
Tim Perkins <tprk77@gmail.com>
2017-01-23 17:47:57 -0500

    Script to add KeePass2

---
+++ ubuntu/add_keepass2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Add PPA if we need it
+ppa_list=$(grep -RoPish '(?<=ppa.launchpad.net/)[^/]+/[^/ ]+' /etc/apt | sort -u | sed 's/^/ppa:/')
+if ! grep "ppa:jtaylor/keepass" <<< "$ppa_list"; then
+    sudo add-apt-repository ppa:jtaylor/keepass
+    sudo apt-get update
+fi
+
+# Install Keepass2 (mono-complete is technically for the Keefox plugin)
+sudo apt-get install -y keepass2 mono-complete
+
+# Install Keefox plugin, if it's there
+keefox_plgx=$(find ~/.mozilla/ -name "KeePassRPC.plgx" | head -n 1)
+if [ -n "$keefox_plgx" ]; then
+    # Remove capitalized directory if it exists
+    if [ -d /usr/lib/keepass2/Plugins ]; then
+        sudo rmdir /usr/lib/keepass2/Plugins
+    fi
+    # Install plugins
+    sudo mkdir -p /usr/lib/keepass2/plugins
+    sudo cp "$keefox_plgx" /usr/lib/keepass2/plugins/KeePassRPC.plgx
+    echo "Installed Keefox plugin, please restart Firefox"
+else
+    echo "Could not find Keefox plugin"
+fi

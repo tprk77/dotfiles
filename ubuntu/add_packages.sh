681d54a424ec5c852f2435ad6a32f24495b92f62
Tim Perkins <tprk77@gmail.com>
2017-01-31 16:22:00 -0500

    Added exFAT utilities.

--- ubuntu/add_packages.sh
+++ ubuntu/add_packages.sh
@@ -7,9 +7,11 @@ general_utils="lftp tree curl sshfs"
 dev_tools="silversearcher-ag dos2unix"
 build_tools="cmake autoconf automake libtool"
 zip_tools="p7zip-full p7zip-rar"
+fs_utils="exfat-fuse exfat-utils"
 
 sudo apt-get install -y \
      $general_utils \
      $dev_tools \
      $build_tools \
-     $zip_tools
+     $zip_tools \
+     $fs_utils

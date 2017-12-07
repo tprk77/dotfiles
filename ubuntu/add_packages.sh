e16c8f70cb2264304d2235e4d8619039046b9ad7
Tim Perkins <tprk77@gmail.com>
2017-12-07 11:58:45 -0500

    Added jq utility.

--- ubuntu/add_packages.sh
+++ ubuntu/add_packages.sh
@@ -3,7 +3,7 @@
 # Installs some common packages with `apt-get`. Assumes that
 # the software sources have been setup appropriately.
 
-general_utils="lftp tree curl sshfs"
+general_utils="lftp tree curl jq sshfs"
 dev_tools="silversearcher-ag dos2unix"
 build_tools="cmake autoconf automake libtool"
 zip_tools="p7zip-full p7zip-rar"

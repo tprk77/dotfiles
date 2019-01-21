86decf4389a2aed7ddd69f4e4f64eb2233e613f8
Tim Perkins <tprk77@gmail.com>
2019-01-21 12:48:34 -0500

    Added Clang for Irony mode

--- ubuntu/add_packages.sh
+++ ubuntu/add_packages.sh
@@ -8,10 +8,12 @@ dev_tools="silversearcher-ag dos2unix"
 build_tools="cmake autoconf automake libtool"
 zip_tools="p7zip-full p7zip-rar"
 fs_utils="exfat-fuse exfat-utils"
+emacs_utils="libclang-6.0-dev"
 
 sudo apt-get install -y \
      $general_utils \
      $dev_tools \
      $build_tools \
      $zip_tools \
-     $fs_utils
+     $fs_utils \
+     $emacs_utils

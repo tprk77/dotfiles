6b3434e68e4d5e53f31b607c2657e1fcd5a18eeb
Tim Perkins <tprk77@gmail.com>
2019-01-21 12:48:34 -0500

    Cleaned up rename script

--- ubuntu/rename_user_dirs.sh
+++ ubuntu/rename_user_dirs.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+IFS=$'\n\t'
+
 # This script takes the annoyingly names default dirs, such as
 # `Documents` and `Downloads`, and renames them to be easy to type and
 # more consistent, like `docs` and `dls`. For best results, run on a
 # new system I guess.
+
+OLD_DESKTOP_NAME="Desktop"
+OLD_DOCUMENTS_NAME="Documents"
+OLD_DOWNLOADS_NAME="Downloads"
+OLD_MUSIC_NAME="Music"
+OLD_PICTURES_NAME="Pictures"
+OLD_PUBLIC_NAME="Public"
+OLD_TEMPLATES_NAME="Templates"
+OLD_VIDEOS_NAME="Videos"
+
+OLD_NAMES=(${OLD_DESKTOP_NAME} ${OLD_DOCUMENTS_NAME} ${OLD_DOWNLOADS_NAME} ${OLD_MUSIC_NAME}
+           ${OLD_PICTURES_NAME} ${OLD_PUBLIC_NAME} ${OLD_TEMPLATES_NAME} ${OLD_VIDEOS_NAME})
 
 BETTER_DESKTOP_NAME="desktop"
 BETTER_DOCUMENTS_NAME="docs"
@@ -14,47 +31,33 @@ BETTER_PUBLIC_NAME="public"
 BETTER_TEMPLATES_NAME="templates"
 BETTER_VIDEOS_NAME="vids"
 
-mv -n ~/Desktop -T ~/${BETTER_DESKTOP_NAME}
-mv -n ~/Documents -T ~/${BETTER_DOCUMENTS_NAME}
-mv -n ~/Downloads -T ~/${BETTER_DOWNLOADS_NAME}
-mv -n ~/Music -T ~/${BETTER_MUSIC_NAME}
-mv -n ~/Pictures -T ~/${BETTER_PICTURES_NAME}
-mv -n ~/Public -T ~/${BETTER_PUBLIC_NAME}
-mv -n ~/Templates -T ~/${BETTER_TEMPLATES_NAME}
-mv -n ~/Videos -T ~/${BETTER_VIDEOS_NAME}
+BETTER_NAMES=(${BETTER_DESKTOP_NAME} ${BETTER_DOCUMENTS_NAME} ${BETTER_DOWNLOADS_NAME}
+              ${BETTER_MUSIC_NAME} ${BETTER_PICTURES_NAME} ${BETTER_PUBLIC_NAME}
+              ${BETTER_TEMPLATES_NAME} ${BETTER_VIDEOS_NAME})
 
-# If we couldn't move them, try deleting
-rmdir --ignore-fail-on-non-empty ~/Desktop
-rmdir --ignore-fail-on-non-empty ~/Documents
-rmdir --ignore-fail-on-non-empty ~/Downloads
-rmdir --ignore-fail-on-non-empty ~/Music
-rmdir --ignore-fail-on-non-empty ~/Pictures
-rmdir --ignore-fail-on-non-empty ~/Public
-rmdir --ignore-fail-on-non-empty ~/Templates
-rmdir --ignore-fail-on-non-empty ~/Videos
+echo "Moving home directories"
+for index in ${!OLD_NAMES[*]}; do
+    old_name=${OLD_NAMES[${index}]}
+    better_name=${BETTER_NAMES[${index}]}
+    [ -d "${HOME}/${old_name}" ] && mv -n "${HOME}/${old_name}" -T "${HOME}/${better_name}"
+    # Try to delete empty directories if we couldn't move them
+    [ -d "${HOME}/${old_name}" ] && rmdir --ignore-fail-on-non-empty "${HOME}/${old_name}"
+done
 
-if [ -f ~/.config/user-dirs.dirs ]; then
-    sed -e "s;Desktop;${BETTER_DESKTOP_NAME};" \
-        -e "s;Documents;${BETTER_DOCUMENTS_NAME};" \
-        -e "s;Downloads;${BETTER_DOWNLOADS_NAME};" \
-        -e "s;Music;${BETTER_MUSIC_NAME};" \
-        -e "s;Pictures;${BETTER_PICTURES_NAME};" \
-        -e "s;Public;${BETTER_PUBLIC_NAME};" \
-        -e "s;Templates;${BETTER_TEMPLATES_NAME};" \
-        -e "s;Videos;${BETTER_VIDEOS_NAME};" \
-        -i ~/.config/user-dirs.dirs
-fi
+echo "Updating ~/.config/user-dirs.dirs"
+for index in ${!OLD_NAMES[*]}; do
+    old_name=${OLD_NAMES[${index}]}
+    better_name=${BETTER_NAMES[${index}]}
+    sed -e "s;\${HOME}/${old_name};\${HOME}/${better_name};" \
+        -i "${HOME}/.config/user-dirs.dirs"
+done
 
-if [ -f ~/.config/gtk-3.0/bookmarks ]; then
-    sed -e "s;Desktop;${BETTER_DESKTOP_NAME};" \
-        -e "s;Documents;${BETTER_DOCUMENTS_NAME};" \
-        -e "s;Downloads;${BETTER_DOWNLOADS_NAME};" \
-        -e "s;Music;${BETTER_MUSIC_NAME};" \
-        -e "s;Pictures;${BETTER_PICTURES_NAME};" \
-        -e "s;Public;${BETTER_PUBLIC_NAME};" \
-        -e "s;Templates;${BETTER_TEMPLATES_NAME};" \
-        -e "s;Videos;${BETTER_VIDEOS_NAME};" \
-        -i ~/.config/gtk-3.0/bookmarks
-fi
+echo "Updating ~/.config/gtk-3.0/bookmarks"
+for index in ${!OLD_NAMES[*]}; do
+    old_name=${OLD_NAMES[${index}]}
+    better_name=${BETTER_NAMES[${index}]}
+    sed -e "s;${HOME}/${old_name};${HOME}/${better_name};" \
+        -i "${HOME}/.config/gtk-3.0/bookmarks"
+done
 
-echo "You need to logout and login to finish renaming user directories!"
+echo "You need to login again for changes to take effect!"
